ff8f0e88042a2d7495d4d059648b27ffc915a8d7
annabency <156775715+annabency@users.noreply.github.com>
2024-06-08 05:27:42 +0000

    7

---
+++ app.py
@@ -0,0 +1,112 @@
+from flask import Flask, request, render_template
+import pickle
+import pandas as pd
+import numpy as np
+
+ld=pd.read_csv('/workspaces/loan_defaulter/Loan_Defaulters - Copy.csv')
+
+app = Flask(__name__)
+
+with open('model1.pkl', 'rb') as file:
+    model1 = pickle.load(file)
+
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    loan_amount=request.form['Loan Amount']
+
+    funded_amount=request.form['Funded Amount']
+
+    funded_amount_investor=request.form['Funded Amount Investor']
+
+    term=request.form['Term']
+
+    interest_rate =request.form['Interest Rate']
+
+    Grade =request.form['Grade']
+
+    employment_duration=request.form['Employment Duration']
+
+    home_ownership=request.form['Home Ownership']
+
+    verification_status=request.form['Verification Status']
+
+    loan_title=request.form['Loan Title']
+
+    debit_to_income=request.form['Debit to Income']
+
+    delinquency_two_years=request.form['Delinquency - two years']
+
+    revolving_balance=request.form['Revolving Balance']
+
+    revolving_utilities=request.form['Revolving Utilities']
+
+    total_accounts=request.form['Total Accounts']
+
+    initial_list_status=request.form['Initial List Status']
+
+    total_received_interest=request.form['Total Received Interest']
+
+    total_received_late_fee=request.form['Total Received Late Fee']
+
+    recoveries=request.form['Recoveries']
+
+    collection_recovery_fee=request.form['Collection Recovery Fee']
+
+    collection_12months_medical=request.form['Collection 12 months Medical']
+
+    application_type=request.form['Application Type']
+
+    last_week_pay=request.form['Last week Pay']
+
+    total_collection_amount=request.form['Total Collection Amount']
+
+    total_current_balance=request.form['Total Current Balance']
+
+    total_revolving_credit=request.form['Total Revolving Credit Limit']
+
+    # Create DataFrame from form data
+    df = pd.DataFrame({
+        'Loan Amount': [float(loan_amount)],
+        'Funded Amount': [float(funded_amount)],
+        'Funded Amount Investor': [float(funded_amount_investor)],
+        'Term': [float(term)],
+        'Interest Rate': [float(interest_rate)],
+        'Grade': [float(Grade)],
+        'Employment Duration': [float(employment_duration)],
+        'Home Ownership': [float(home_ownership)],
+        'Verification Status': [float(verification_status)],
+        'Loan Title': [float(loan_title)],
+        'Debit to Income': [float(debit_to_income)],
+        'Delinquency - two years': [float(delinquency_two_years)],
+        'Revolving Balance': [float(revolving_balance)],
+        'Revolving Utilities': [float(revolving_utilities)],
+        'Total Accounts': [float(total_accounts)],
+        'Initial List Status': [float(initial_list_status)],
+        'Total Received Interest': [float(total_received_interest)],
+        'Total Received Late Fee': [float(total_received_late_fee)],
+        'Recoveries': [float(recoveries)],
+        'Collection Recovery Fee': [float(collection_recovery_fee)],
+        'Collection 12 months Medical': [float(collection_12months_medical)],
+        'Application Type': [float(application_type)],
+        'Last week Pay': [float(last_week_pay)],
+        'Total Collection Amount': [float(total_collection_amount)],
+        'Total Current Balance': [float(total_current_balance)],
+        'Total Revolving Credit Limit': [float(total_revolving_credit)]
+    })
+
+
+
+    prediction = model1.predict(df)
+    print(prediction)
+    prediction_result='Defaulter' if prediction[0]== 1 else 'Non-Defaulter'
+
+    return render_template('result.html',pred=prediction_result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ loan.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+
+
+ld=pd.read_csv('/workspaces/loan_defaulter/Loan_Defaulters - Copy.csv')
+ld=ld.drop(['ID'],axis=1)
+
+ld['Verification Status']=ld['Verification Status'].replace('Source Verified','Verified')
+
+#Outliers
+q1=np.percentile(ld['Funded Amount Investor'],25,method='midpoint')
+q2=np.percentile(ld['Funded Amount Investor'],50,method='midpoint')
+q3=np.percentile(ld['Funded Amount Investor'],75,method='midpoint')
+IQR=q3-q1
+low_lim=q1-1.5*IQR
+up_lim=q3+1.5*IQR
+ld['Funded Amount Investor']=ld['Funded Amount Investor'].clip(lower=low_lim,upper=up_lim)
+
+
+q1=np.percentile(ld['Revolving Balance'],25,method='midpoint')
+q2=np.percentile(ld['Revolving Balance'],50,method='midpoint')
+q3=np.percentile(ld['Revolving Balance'],75,method='midpoint')
+IQR=q3-q1
+low_lim=q1-1.5*IQR
+up_lim=q3+1.5*IQR
+ld['Revolving Balance']=ld['Revolving Balance'].clip(lower=low_lim,upper=up_lim)
+
+q1=np.percentile(ld['Total Accounts'],25,method='midpoint')
+q2=np.percentile(ld['Total Accounts'],50,method='midpoint')
+q3=np.percentile(ld['Total Accounts'],75,method='midpoint')
+IQR=q3-q1
+low_lim=q1-1.5*IQR
+up_lim=q3+1.5*IQR
+ld['Total Accounts']=ld['Total Accounts'].clip(lower=low_lim,upper=up_lim)
+
+q1=np.percentile(ld['Total Received Late Fee'],25,method='midpoint')
+q2=np.percentile(ld['Total Received Late Fee'],50,method='midpoint')
+q3=np.percentile(ld['Total Received Late Fee'],75,method='midpoint')
+IQR=q3-q1
+low_lim=q1-1.5*IQR
+up_lim=q3+1.5*IQR
+ld['Total Received Late Fee']=ld['Total Received Late Fee'].clip(lower=low_lim,upper=up_lim)
+
+q1=np.percentile(ld['Total Collection Amount'],25,method='midpoint')
+q2=np.percentile(ld['Total Collection Amount'],50,method='midpoint')
+q3=np.percentile(ld['Total Collection Amount'],75,method='midpoint')    
+IQR=q3-q1
+low_lim=q1-1.5*IQR
+up_lim=q3+1.5*IQR
+ld['Total Collection Amount']=ld['Total Collection Amount'].clip(lower=low_lim,upper=up_lim)
+
+#Label encoding
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+
+ld=ld.drop(['Sub Grade','Batch Enrolled'],axis=1)
+
+ld['Grade']=le.fit_transform(ld['Grade'])
+ld['Employment Duration']=le.fit_transform(ld['Employment Duration'])
+ld['Verification Status']=le.fit_transform(ld['Verification Status'])
+ld['Loan Title']=le.fit_transform(ld['Loan Title'])
+ld['Initial List Status']=le.fit_transform(ld['Initial List Status'])
+ld['Application Type']=le.fit_transform(ld['Application Type'])
+
+#Test & train data
+y=ld['Loan Status']
+X=ld.drop(['Loan Status'],axis=1)
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=42,test_size=0.25)
+
+#model random forest
+from sklearn.ensemble import RandomForestClassifier
+rf_clf=RandomForestClassifier()
+rf_clf.fit(X_train,y_train)
+y_pred=rf_clf.predict(X_test)
+
+#create pickle
+import pickle
+with open('model1.pkl','wb') as model1_file:  #wb-- write as binary
+    pickle.dump(rf_clf,model1_file) 
+
